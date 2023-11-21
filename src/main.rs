4efbab0a046f32cf2af7928da7cadd5d5b4cf5f0
Pelfox <iflegish8@yandex.ru>
2023-11-21 23:32:07 +0300

    read data from stream

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-use tokio::{io::AsyncWriteExt, net::TcpStream};
+use tokio::{io::{AsyncWriteExt, AsyncReadExt}, net::TcpStream};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:3444").await?;
-    stream.write_u8(1).await?;
-    stream.write_u8(0).await?;
+    
+    let mut buffer = vec![];
+    buffer.write_u8(0).await?; // packet id
+    
+    // packets fields
+    let version = String::from("1.0.0"); // client version (1.0.0)
+    buffer.write_u8(version.len() as u8).await?;
+    buffer.write_all(version.as_bytes()).await?;
 
-    Ok(())
+    buffer.write_u8(0).await?; // enable compression (false)
+    buffer.write_i64(-1).await?; // compression threshold (-1)
+    
+    stream.write_u8(buffer.len() as u8).await?; // packet length
+    stream.write_all(&buffer).await?;
+
+    loop {
+        let v = stream.read_u8().await?;
+        println!("{:#?}", v);
+    }
 }
